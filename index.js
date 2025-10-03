bef111bf5c07613a55ec194481d293af4f50f82c
greemaltend4 <dengestabiy@hotmail.com>
2025-10-03 16:19:46 +0000

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue dans l'application Intégration Cloud Analytics !');
+});
+
+app.listen(PORT, () => {
+    console.log(`Serveur en cours d\u2019exécution sur le port ${PORT}`);
+});
